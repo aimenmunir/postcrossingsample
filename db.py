7b962029bf3988e9b01b56d2c98861d5e83ceb70
Aimen Munir <aimenmunir001@gmail.com>
2025-11-17 19:01:46 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,17 +1,14 @@
-# db.py  (optional replacement, only if you add .env)
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME", "postcrossing")
-
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
-
 try:
     db.command("ping")
-    print("✅ Connected to MongoDB Atlas successfully!")
+    print("Connected to MongoDB Atlas successfully!")
 except Exception as e:
-    print("❌ Connection failed:", e)
+    print("Connection failed:", e)
+
